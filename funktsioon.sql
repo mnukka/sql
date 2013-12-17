fa24c2d80bd17525e71850a62b9736072f32f464
mnukka <mnukka@gmail.com>
2013-12-17 17:22:53 +0200

    eemaldasin muutuja

--- funktsioon.sql
+++ funktsioon.sql
@@ -4,15 +4,12 @@
 -- Create date: 2013 detsember
 -- Description:	Võrdleb mitme protsendivõrra kasvas B võrreldes A'ga
 -- =============================================
-CREATE FUNCTION PercentageIncrease(Lugeja decimal, Nimetaja decimal) RETURNS decimal AS $$
-DECLARE 
-	output decimal; --Error sellel real? why...
+CREATE FUNCTION PercentageIncrease(Lugeja decimal, Nimetaja decimal) RETURNS DECIMAL AS $$
 BEGIN
 	IF Nimetaja = 0 
 		THEN RETURN 0;
 	ELSE
-		output := ROUND((Lugeja - Nimetaja) / Nimetaja * 100, 2);
+		RETURN ROUND((Lugeja - Nimetaja) / Nimetaja * 100, 2);
 	END IF;
-	RETURN output;
 END;
 $$ language SQL;
