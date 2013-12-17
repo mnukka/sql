149a99073bd4b3c39271cbb5e0e746fccebaa200
mnukka <mnukka@gmail.com>
2013-12-18 01:29:56 +0200

    korras

---
+++ daily_data_table_structure.sql
@@ -0,0 +1,37 @@
+﻿-- Table: "DailyData"
+
+-- DROP TABLE "DailyData";
+
+CREATE TABLE "DailyData"
+(
+  data_id bigint,
+  name text,
+  date_time timestamp without time zone,
+  dayn integer,
+  hour integer,
+  average_buy numeric(20,4),
+  average_sell numeric(20,4),
+  previous_average_buy numeric(20,4),
+  previous_average_sell numeric(20,4),
+  buy_count integer,
+  sell_count integer,
+  "ID" serial NOT NULL,
+  CONSTRAINT "DailyData_pkey" PRIMARY KEY ("ID")
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "DailyData"
+  OWNER TO "ServerUser";
+COMMENT ON TABLE "DailyData"
+  IS 'Igapäevased andmed tundide lõikes';
+
+-- Index: "DailyData_data_id_idx"
+
+-- DROP INDEX "DailyData_data_id_idx";
+
+CREATE INDEX "DailyData_data_id_idx"
+  ON "DailyData"
+  USING btree
+  (data_id);
+
--- updateHourly.sql
+++ updateHourly.sql
@@ -10,17 +10,13 @@ SELECT
 
                 CAST(AVG(prices.buy_price) AS float4) AS average_Buy,
                 CAST(AVG(prices.sell_price) AS float4) AS average_Sell,
-                LAG(CAST(        AVG(prices.buy_price) AS float4))        OVER w AS last_buy,
-                LAG(CAST(        AVG(prices.sell_price) AS float4))         OVER w AS last_sell,
+                LAG(CAST(        AVG(prices.buy_price) AS float4))        OVER (ORDER BY date_part('hour', prices.date)) AS last_buy,
+                LAG(CAST(        AVG(prices.sell_price) AS float4))         OVER (ORDER BY date_part('hour', prices.date)) AS last_sell,
                 AVG(sell_count),
                 AVG(buy_count)
                         FROM "GuildWars2_prices" AS prices
                         GROUP BY
-                                prices.data_id,
-				prices.buy_price,
-				prices.sell_price,
                                 date_part('day', prices.date),
-                                date_part('hour', prices.date)
-                        WINDOW w AS (PARTITION BY prices.buy_price, prices.sell_price ORDER BY date_part('hour', prices.date));
+                                date_part('hour', prices.date);
 $BODY$
 LANGUAGE SQL
